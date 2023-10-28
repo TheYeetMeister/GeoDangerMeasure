78fe8c7b092a1dbf9e2bec06ebe726e2852ad5f1
Karson Tran <karsont@uci.edu>
2023-10-28 10:40:58 -0700

    fix spelling error

--- mappingApi.py
+++ mappingApi.py
@@ -1,35 +1,19 @@
 import requests
 import json
 
-url = "https://app.zipcodebase.com/api/v1/code/city"
-
-params = (
-    ("city", "San Francisco"),
-    ("state_name","California"),
-    ("country","us")
-)
-
-headers = {
-    "apikey": "43ef42a0-75af-11ee-b46e-c76fdca57988"
-}
-
-response = requests.get(url, headers=headers, params=params)
-responseJson = json.loads(response.text)
-print(ZipFetch("43ef42a0-75af-11ee-b46e-c76fdca57988"))
-
 class ZipFetch:
     def __init__(self, apikey :str):
-        headers :dict = {
-            "apikey": "43ef42a0-75af-11ee-b46e-c76fdca57988"
+        self._headers :dict = {
+            "apikey": apikey
         }
 
     def getZipResponse(self, url :str, params :tuple) -> list[int]:
-        response = requests.get(url, headers=headers, params=params)
+        response = requests.get(url, headers=self._headers, params=params)
         responseJson = json.loads(response.text)
         response.close()
         return responseJson["results"]
 
-    def getZipByCity(self, city :str, state_name :"optionalStr", country :str) -> list[int]:
+    def getZipByCity(self, city :str, country :str, state_name :"optionalStr" = "") -> list[int]:
         url :str = "https://app.zipcodebase.com/api/v1/code/city"
 
         if(state_name):
@@ -44,6 +28,6 @@ class ZipFetch:
                 ("country", country)
             )
 
-        return self.getZipResonse(url, params)
+        return self.getZipResponse(url, params)
 
-        
+print(ZipFetch("43ef42a0-75af-11ee-b46e-c76fdca57988").getZipByCity("San Francisco", "us")) 
