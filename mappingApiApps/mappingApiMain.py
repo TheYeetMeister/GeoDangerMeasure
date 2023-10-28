de67cf99c54a955ea74503915a95fe090859704c
Karson Tran <karsont@uci.edu>
2023-10-28 11:16:04 -0700

    change variable name add string calification

--- mappingApiApps/mappingApiMain.py
+++ mappingApiApps/mappingApiMain.py
@@ -8,6 +8,15 @@ def getRadiusZips() -> list[int]:
 
     return mappingApi.ZipFetch(apikey).getZipByRadius(originZip, radius, countryCode)
 
+def getCityZips() -> list[int]:
+    apikey :str = input("What is your Apikey? :")
+    city :str = input("What is the city where you want to collect zips? :")
+    stateCode :str = input("What is the state where you want to collect zips?\n (This is optional you can just input an empty response, please respond with a state abbreviation) :")
+    countryCode :str = input("What is the country code of the country where you want to collect zips? :")
+
+    return mappingApi.ZipFetch(apikey).getZipByCity(city, countryCode, stateCode)
+
+
 def main():
     output :list[int] = []
 
