7b3d561061cd36b637d7668cb9aca5a6de3c81a6
Karson Tran <karsont@uci.edu>
2023-10-28 10:28:03 -0700

    create class

--- mappingApi.py
+++ mappingApi.py
@@ -17,6 +17,12 @@ response = requests.get(url, headers=headers, params=params)
 responseJson = json.loads(response.text)
 print(responseJson["results"])
 
+class ZipFetch:
+    def __init__(self, apikey :str):
+        headers :dict = {
+            "apikey": "43ef42a0-75af-11ee-b46e-c76fdca57988"
+        }
+        
 def getZipByCity(city :str, state_name :"optionalStr", country :str) -> list[int]:
     if(state_name):
         params = (
