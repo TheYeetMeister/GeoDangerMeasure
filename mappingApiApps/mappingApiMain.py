a2a636baa8b7d5b931008dcab1f82e1399d3f26d
Karson Tran <karsont@uci.edu>
2023-10-28 11:06:43 -0700

    create if elif statement

--- mappingApiApps/mappingApiMain.py
+++ mappingApiApps/mappingApiMain.py
@@ -4,5 +4,11 @@ def main():
     print("Would you like to get zip codes by radius, or by city?")
     choice = input("response with \"radius\" or \"city\"\n").upper()
 
+    if(choice == "RADIUS"):
+
+    elif(choice == "city"):
+
+    
+
 if __name__ == "__main__":
     main()
