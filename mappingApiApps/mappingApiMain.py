41ed80375d50d833f8d1d040e037e02895a6eff4
Karson Tran <karsont@uci.edu>
2023-10-28 11:03:28 -0700

    get user input

--- mappingApiApps/mappingApiMain.py
+++ mappingApiApps/mappingApiMain.py
@@ -2,7 +2,7 @@ import mappingApi
 
 def main():
     print("Would you like to get zip codes by radius, or by city?")
-    input("response with \"radius\" or \"city\"\n")
+    choice = input("response with \"radius\" or \"city\"\n").upper()
 
 if __name__ == "__main__":
     main()
